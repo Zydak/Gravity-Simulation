e21a4d1e64f1c611614a4927ee238648fdd47cac
Zydak <koksszmoks@gmail.com>
2023-05-06 21:18:03 +0200

    Windows setup

---
+++ VulkanVs/VulkanVs/src/cameraController.cpp
@@ -0,0 +1,92 @@
+#include "cameraController.h"
+
+#include <iostream>
+
+#include "imgui.h"
+#define _USE_MATH_DEFINES
+#include "glm/glm.hpp"
+
+static double yaw = 0;
+static double pitch = 89;
+static double lastX;
+static double lastY;
+
+static void mouseCallback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_LEFT) 
+    {
+        double x, y;
+        glfwGetCursorPos(window, &x, &y);
+        if(GLFW_PRESS == action)
+        {
+            lastX = x;
+            lastY = y;
+        }
+        else if(GLFW_RELEASE == action)
+        {
+        }
+    }
+}
+static double scrollY = 0.00000000000000001;
+void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	if (yoffset > 0)
+    	scrollY -= scrollY / 4;
+	if (yoffset < 0)
+    	scrollY += scrollY / 4;
+
+    if (scrollY >= 1.24701e+09)
+        scrollY = 1.24701e+09;
+}
+
+CameraController::CameraController(GLFWwindow* window)
+    : m_Window(window)
+{
+    glfwSetScrollCallback(m_Window, ScrollCallback);
+    glfwSetMouseButtonCallback(m_Window, mouseCallback);
+}
+
+CameraController::~CameraController()
+{
+    
+}
+
+/*
+    * @brief Updates camera position based on mouse movement
+*/
+void CameraController::Update(const float& delta, Camera& camera, const int& target, Map& gameObjects)
+{
+    if (scrollY < gameObjects[target]->GetObjectProperties().radius*2/SCALE_DOWN)
+        scrollY = gameObjects[target]->GetObjectProperties().radius*2/SCALE_DOWN;
+    double radius = scrollY;
+    if (glfwGetMouseButton(m_Window, 0) == GLFW_PRESS && !ImGui::GetIO().WantCaptureMouse)
+    {
+        double x, y;
+        glfwGetCursorPos(m_Window, &x, &y);
+            
+        double sensitivity = -10.0f;
+        static double xOffset = 0;
+        static double yOffset = 0;
+        xOffset = (lastX - x) * sensitivity;
+        yOffset = (lastY - y) * sensitivity;
+        yaw += xOffset * delta;
+        pitch += yOffset * delta;
+
+        if (pitch > 89)
+            pitch = 89;
+        if (pitch < -89)
+            pitch = -89;
+
+        lastX = x;
+        lastY = y;
+    }
+    
+    // Equation for camera positioning around a sphere
+    camera.m_Transform.translation.x = radius * -sin(yaw*(3.1415926535897932384626433832795 /180.0)) * cos((pitch)*(3.1415926535897932384626433832795 /180.0));
+    camera.m_Transform.translation.y = radius * -sin((pitch)*(3.1415926535897932384626433832795 /180.0));
+    camera.m_Transform.translation.z = -radius * cos((yaw)*(3.1415926535897932384626433832795 /180.0)) * cos((pitch)*(3.1415926535897932384626433832795 /180.0));
+
+    // only for debuging info
+    camera.m_Transform.rotation.x = yaw;
+    camera.m_Transform.rotation.y = pitch;
+}
---
+++ VulkanVs/VulkanVs/src/models/skyboxModel.cpp
@@ -0,0 +1,235 @@
+#include "skyboxModel.h"
+#include "../vulkan/utils.h"
+
+#include <cstring>
+#include <unordered_map>
+
+#include "tinyobjloader/tiny_obj_loader.h"
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+#include <stdexcept>
+
+namespace std
+{
+    template<>
+    struct hash<SkyboxModel::Vertex>
+    {
+        size_t operator()(SkyboxModel::Vertex const& vertex) const
+        {
+            size_t seed = 0;
+            HashCombine(seed, vertex.position);
+            return seed;
+        }
+    };
+}
+
+SkyboxModel::SkyboxModel(Device& device, const SkyboxModel::Builder& builder)
+    : m_Device(device)
+{
+    CreateVertexBuffer(builder.vertices);
+    CreateIndexBuffer(builder.indices);
+}
+SkyboxModel::~SkyboxModel()
+{
+
+}
+
+void SkyboxModel::CreateVertexBuffer(const std::vector<Vertex> &vertices)
+{
+    m_VertexCount = static_cast<uint32_t>(vertices.size());
+    //assert(m_VertexCount >= 3 && "Vertex count me be at least 3");
+    VkDeviceSize bufferSize = sizeof(vertices[0]) * m_VertexCount;
+    uint32_t vertexSize = sizeof(vertices[0]);
+
+    /*
+        We need to be able to write our vertex data to memory.
+        This property is indicated with VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT with VK_MEMORY_PROPERTY_HOST_COHERENT_BIT property.
+        VK_MEMORY_PROPERTY_HOST_COHERENT_BIT is Used to get memory heap that is host coherent. 
+        We use this to copy the data into the buffer memory immediately.
+    */
+    Buffer stagingBuffer(m_Device, 
+        vertexSize, 
+        m_VertexCount,
+        VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+    /*
+        When buffer is created It is time to copy the vertex data to the buffer. 
+        This is done by mapping the buffer memory into CPU accessible memory with vkMapMemory.
+    */
+    stagingBuffer.Map();
+    stagingBuffer.WriteToBuffer((void*)vertices.data());
+
+    /*
+        The vertexBuffer is now allocated from a memory type that is device 
+        local, which generally means that we're not able to use vkMapMemory. 
+        However, we can copy data from the stagingBuffer to the vertexBuffer. 
+        We have to indicate that we intend to do that by specifying the transfer 
+        source flag(VK_BUFFER_USAGE_TRANSFER_SRC_BIT) for the stagingBuffer 
+        and the transfer destination flag(VK_BUFFER_USAGE_TRANSFER_DST_BIT)
+        for the vertexBuffer, along with the vertex buffer usage flag.
+    */
+
+    m_VertexBuffer = std::make_unique<Buffer>(
+        m_Device,
+        vertexSize,
+        m_VertexCount,
+        VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, 
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT
+    );
+
+    m_Device.CopyBuffer(stagingBuffer.GetBuffer(), m_VertexBuffer->GetBuffer(), bufferSize);
+}
+
+void SkyboxModel::CreateIndexBuffer(const std::vector<uint32_t> &indices)
+{
+    m_IndexCount = static_cast<uint32_t>(indices.size());
+    m_HasIndexBuffer = m_IndexCount > 0;
+    if (!m_HasIndexBuffer)
+    {
+        return;
+    }
+
+    VkDeviceSize bufferSize = sizeof(indices[0]) * m_IndexCount;
+    uint32_t indexSize = sizeof(indices[0]);
+
+    /*
+        We need to be able to write our index data to memory.
+        This property is indicated with VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT with VK_MEMORY_PROPERTY_HOST_COHERENT_BIT property.
+        VK_MEMORY_PROPERTY_HOST_COHERENT_BIT is Used to get memory heap that is host coherent. 
+        We use this to copy the data into the buffer memory immediately.
+    */
+    Buffer stagingBuffer(
+        m_Device,
+        indexSize,
+        m_IndexCount,
+        VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT
+    );
+
+    /*
+        When buffer is created It is time to copy the index data to the buffer. 
+        This is done by mapping the buffer memory into CPU accessible memory with vkMapMemory.
+    */
+    stagingBuffer.Map();
+    stagingBuffer.WriteToBuffer((void*)indices.data());
+
+    /*
+        The IndexBuffer is now allocated from a memory type that is device 
+        local, which generally means that we're not able to use vkMapMemory. 
+        However, we can copy data from the stagingBuffer to the IndexBuffer. 
+        We have to indicate that we intend to do that by specifying the transfer 
+        source flag(VK_BUFFER_USAGE_TRANSFER_SRC_BIT) for the stagingBuffer 
+        and the transfer destination flag(VK_BUFFER_USAGE_TRANSFER_DST_BIT)
+        for the IndexBuffer, along with the IndexBuffer usage flag.
+    */
+    m_IndexBuffer = std::make_unique<Buffer>(
+        m_Device,
+        indexSize,
+        m_IndexCount,
+        VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, 
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT
+    );
+
+    m_Device.CopyBuffer(stagingBuffer.GetBuffer(), m_IndexBuffer->GetBuffer(), bufferSize);
+}
+
+/*
+    @brief this function call vkCmdBindVertexBuffers which is used to bind vertex buffers to bindings
+*/
+void SkyboxModel::Bind(VkCommandBuffer commandBuffer)
+{
+    VkBuffer buffers[] = {m_VertexBuffer->GetBuffer()};
+    VkDeviceSize offsets[] = {0};
+    vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
+
+    if (m_HasIndexBuffer)
+    {
+        vkCmdBindIndexBuffer(commandBuffer, m_IndexBuffer->GetBuffer(), 0, VK_INDEX_TYPE_UINT32);
+    }
+}
+
+void SkyboxModel::Draw(VkCommandBuffer commandBuffer)
+{
+    if (m_HasIndexBuffer)
+    {
+        vkCmdDrawIndexed(commandBuffer, m_IndexCount, 1, 0, 0, 0);
+    }
+    else
+    {
+        vkCmdDraw(commandBuffer, m_VertexCount, 1, 0, 0);
+    }
+}
+
+std::vector<VkVertexInputBindingDescription> SkyboxModel::Vertex::GetBindingDescriptions()
+{
+    std::vector<VkVertexInputBindingDescription> bindingDescription(1);
+    bindingDescription[0].binding = 0;
+    bindingDescription[0].stride = sizeof(Vertex);
+    bindingDescription[0].inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+    return bindingDescription;
+}
+
+std::vector<VkVertexInputAttributeDescription> SkyboxModel::Vertex::GetAttributeDescriptions()
+{
+    std::vector<VkVertexInputAttributeDescription> attributeDescriptions{};
+    attributeDescriptions.push_back({0, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, position)});
+
+    return attributeDescriptions;
+}
+
+std::unique_ptr<SkyboxModel> SkyboxModel::CreateModelFromFile(Device& device, const std::string& modelFilepath)
+{
+    Builder builder{};
+    builder.LoadModel(modelFilepath);
+
+    return std::make_unique<SkyboxModel>(device, builder);
+}
+
+void SkyboxModel::Builder::LoadModel(const std::string& modelFilepath)
+{
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+    std::string warn, err;
+
+    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, modelFilepath.c_str()))
+    {
+        throw std::runtime_error(warn + err);
+    }
+
+    vertices.clear();
+    indices.clear();
+
+    std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+    for (const auto &shape : shapes) 
+    {
+        for (const auto &index : shape.mesh.indices) 
+        {
+            Vertex vertex{};
+
+            if (index.vertex_index >= 0) 
+            {
+                vertex.position = {
+                    attrib.vertices[3 * index.vertex_index + 0],
+                    attrib.vertices[3 * index.vertex_index + 1],
+                    attrib.vertices[3 * index.vertex_index + 2],
+                };
+            }
+
+            if (uniqueVertices.count(vertex) == 0) 
+            {
+                uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+                vertices.push_back(vertex);
+            }
+            indices.push_back(uniqueVertices[vertex]);
+        }
+    }
+}
+
+void SkyboxModel::UpdateVertexBuffer(VkCommandBuffer cmd, Buffer* buffer, const std::vector<Vertex> &vertices)
+{
+    vkCmdUpdateBuffer(cmd, buffer->GetBuffer(), 0, sizeof(vertices[0]) * vertices.size(), vertices.data());
+}
